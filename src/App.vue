bcae531e075d78604567d02f81041d81a82fe3f8
yuriy <meresin.y@outlook.com>
2023-03-04 17:15:42 +0400

    вывел форму для создания посте в модалку, настроил закрытие по клику вне формы, и чтобы не закрывала по клику внутри формы

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,69 @@
 <template>
-  <div class="app">
-    <div>
-      <PostForm @create="createPost" />
-      <PostList :posts="posts" @remove="removePost" />
+    <div class="app">
+        <h1>Страница с постами</h1>
+        <my-button @click="showDialog">Создать пост</my-button>
+        <my-dialog v-model:show="dialogVisible">
+            <PostForm @create="createPost" />
+        </my-dialog>
+        <div>
+            <PostList :posts="posts" @remove="removePost" />
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import PostForm from "@/components/PostForm";
-import PostList from "@/components/PostList";
-export default {
-  components: {
-    PostForm,
-    PostList,
-  },
-  data() {
-    return {
-      posts: [
-        { id: 1, title: "Пост 1 о JavaScript", body: "текст о JavaScript" },
-        { id: 2, title: "Пост 2 о JavaScript", body: "текст о JavaScript" },
-        { id: 3, title: "Пост 3 о JavaScript", body: "текст о JavaScript" },
-      ],
+    import PostForm from "@/components/PostForm";
+    import PostList from "@/components/PostList";
+    export default {
+        components: {
+            PostForm,
+            PostList,
+        },
+        data() {
+            return {
+                posts: [
+                    {
+                        id: 1,
+                        title: "Пост 1 о JavaScript",
+                        body: "текст о JavaScript",
+                    },
+                    {
+                        id: 2,
+                        title: "Пост 2 о JavaScript",
+                        body: "текст о JavaScript",
+                    },
+                    {
+                        id: 3,
+                        title: "Пост 3 о JavaScript",
+                        body: "текст о JavaScript",
+                    },
+                ],
+                dialogVisible: false,
+            };
+        },
+        methods: {
+            createPost(post) {
+                this.posts.push(post);
+                this.dialogVisible = false;
+            },
+            removePost(post) {
+                this.posts = this.posts.filter((p) => p.id !== post.id);
+            },
+            showDialog() {
+                this.dialogVisible = true;
+            },
+        },
     };
-  },
-  methods: {
-    createPost(post) {
-      this.posts.push(post);
-    },
-    removePost(post) {
-      this.posts = this.posts.filter((p) => p.id !== post.id);
-    },
-  },
-};
 </script>
 <style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
 
-.app {
-  padding: 20px;
-}
+    .app {
+        padding: 20px;
+    }
 </style>
 <!-- TIMING 1:10:20 -->
